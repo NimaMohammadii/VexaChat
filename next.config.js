290ef08ef757d22c5aa4a7aa6ba4b0fcf468d979
NimaMohammadii <nima.mohammadi084@gmail.com>
2026-02-21 18:57:49 +0330

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-const supabaseHostname = (() => {
-  try {
-    const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    return url ? new URL(url).hostname : null;
-  } catch {
-    return null;
-  }
-})();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -16,18 +7,6 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
-      {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-      },
-      ...(supabaseHostname
-        ? [
-            {
-              protocol: 'https',
-              hostname: supabaseHostname,
-            },
-          ]
-        : []),
     ],
   },
 };
